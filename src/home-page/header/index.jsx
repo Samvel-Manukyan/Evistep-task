95272b984e3fbdb6db0bd03c668cd3415a776c6c
Samvel <s.manukyan@inorain.com>
2023-01-16 01:06:17 +0400

    first commit

---
+++ src/home-page/header/index.jsx
@@ -0,0 +1,13 @@
+import './index.scss';
+
+const Header = () => {
+  return(
+      <div className="header">
+          <img src="/assets/icons/logo.png" alt="logo"/>
+
+          <button>GET NOW</button>
+      </div>
+  );
+}
+
+export default Header;
---
+++ src/home-page/index.js
@@ -0,0 +1,13 @@
+import Header from './header';
+
+import './index.scss';
+
+const HomePage = () => {
+  return(
+      <div className="home_page">
+          <Header/>
+      </div>
+  );
+}
+
+export default HomePage;
